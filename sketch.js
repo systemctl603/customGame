ffb2ebedb3e2010e847c48efa4b00aec7b082325
system-256 <senthilvel.bagavath@gmail.com>
2020-05-09 23:08:06 +0530

    Added more sprites and taskAllocation.md

--- sketch.js
+++ sketch.js
@@ -1,47 +1,66 @@
-var coordinates = [];
-var canvas;
-var database;
+var player, playerImg;
+var playerLeft, playerRight;
+var bgImage, planetImg, boxImg;
+var showSprites = true;
+var bgstate = 1;
+var box1, box2, box3, box4, box5;
+
+function preload() {
+    playerImg = loadImage('images/astronaut/astronaut-still.png');
+    bgImage = loadImage('images/rocketBackground.png');
+    instructionImage = loadImage('images/first.png');
+    planetImg = loadImage('images/marsBackground.jpeg');
+    boxImg = loadImage('images/box.png');
+    playerLeft = loadAnimation('images/astronaut/walkLeft1.png', 'images/astronaut/walkLeft2.png')
+    playerRight = loadAnimation('images/astronaut/walkRight1.png', 'images/astronaut/walkRight2.png')
+}
 
 function setup() {
-    canvas = createCanvas(800, 400);
-    database = firebase.database();
+    var cnv = createCanvas(windowWidth, windowHeight);
+    player = createSprite(100, windowHeight - 75, 32, 64);
+    player.addImage('still', playerImg);
+    player.addAnimation('playerLeft', playerLeft);
+    player.addAnimation('playerRight', playerRight);
+    player.scale = 1;
+    box1 = new Box(300, windowHeight - 50, () => {
+
+    })
 }
 
 function draw() {
-    sync();
-}
-
-function mouseDragged() {
-    coordinates.push([mouseX, mouseY]);
-    database.ref('array').set(coordinates);
-    for (var i = 0; i < coordinates.length; i++) {
-        var x = coordinates[i][0];
-        var y = coordinates[i][1];
-        //console.log(i);
-        if (i >= 1) {
-            line(x, y, coordinates[i - 1][0], coordinates[i - 1][1])
-        }
+    bg();
+    if (keyIsDown(LEFT_ARROW)) {
+        player.changeAnimation('playerLeft');
+        player.x -= 10;
+    } else if (keyIsDown(RIGHT_ARROW)) {
+        player.changeAnimation('playerRight');
+        player.x += 10;
+    } else {
+        player.changeImage('still');
     }
-    endShape();
+    if (player.collide(box1.sprite)) {
+
+    }
+    if (showSprites == true) { drawSprites(); }
 }
 
-function mouseReleased() {
-    coordinates = [];
+function bg() {
+    if (bgstate == 1) {
+        background(planetImg);
+    } else if (bgstate == 2) {
+        background(bgImage);
+    }
 }
 
-function sync() {
-    var array;
-    var drawCoords = database.ref('array');
-    drawCoords.on('value', function (data) {
-        array = data.val()
-    })
-    console.log(array);
-    for (var i = 0; i < array.length; i++) {
-        var x = array;
-        var y = array;
-        //console.log(i);
-        if (i >= 1) {
-            line(x, y, array[i - 1][0], array[i - 1][1])
-        }
+class Box {
+    constructor(x, y, callback) {
+        this.sprite = createSprite(x, y);
+        this.sprite.scale = 0.5;
+        this.image = boxImg;
+        this.sprite.addImage(this.image);
+    }
+
+    execAction(callback) {
+        callback();
     }
 }
---
+++ taskAllocation.md
@@ -0,0 +1,24 @@
+# Allocation of Tasks
+
+### Story:
+
+The main player finds himself on a foreign planet\
+where he must create a suitable environment\
+for future humans to settle down.\
+But, he must overcome the following:
+
+- A very harsh atmosphere
+- A jungle which contains many monsters
+- The bases which hold people must be built
+
+They will respectively be acheived by:
+
+- An oxygen emittor
+- A sword to take the jungle
+- A mining unit to mine resources
+
+After this, he sends a message to humanity\
+telling them the planet is ready.\
+This ends the game.
+
+### Game Mechanics:
